fa3850943269e1adcf810b7701e4f551e2c01f0b
MaxixV2 <obughnyim@gmail.com>
2024-01-05 05:49:34 +0200

    init commit

---
+++ data/data.py
@@ -0,0 +1,4 @@
+class Data:
+    countries = ["India", "United States", "Canada", "Australia", "Israel", "New Zealand", "Singapore"]
+    valid_email = "maksymlqi@gmail.com"
+    valid_password = "qwerty"
