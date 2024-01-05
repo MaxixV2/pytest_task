c3c93fd8e94efe64c2065f085cb44c0b804d3791
MaxixV2 <obughnyim@gmail.com>
2024-01-05 06:04:52 +0200

    code refactoring

--- data/data.py
+++ data/data.py
@@ -1,4 +1,3 @@
 class Data:
-    countries = ["India", "United States", "Canada", "Australia", "Israel", "New Zealand", "Singapore"]
     valid_email = "maksymlqi@gmail.com"
     valid_password = "qwerty"
